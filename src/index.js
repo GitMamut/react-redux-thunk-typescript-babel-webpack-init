0ded2b7c98e30dda80f50d70f0f7bf9520e683c7
Przemek <vatier@gazeta.pl>
2019-02-17 12:29:37 +0100

    migrate to webpack with babel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import FormContainer from "./js/components/container/FormContainer.jsx";
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
